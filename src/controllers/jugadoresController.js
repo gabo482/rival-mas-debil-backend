a17af2a6313b1b48ce52de82b0bf5e32134dc5aa
gabo482 <vcruztesci@gmail.com>
2025-11-03 14:15:23 -0600

    Fix borrar

--- src/controllers/jugadoresController.js
+++ src/controllers/jugadoresController.js
@@ -5,7 +5,7 @@ const jugadores = [];
 exports.agregarJugador = (req, res) => {
     console.log("Tipo de contenido:", req.headers['content-type']);
     console.log("Cuerpo recibido:", req.body);
-    res.send("Verificando cuerpo...")
+    console.log("Verificando cuerpo...")
 
     console.log("REQ BODY:", req.body);
     const { numeroEquipo, nombreEquipo, integrantes, icono } = req.body;
@@ -26,3 +26,34 @@ exports.agregarJugador = (req, res) => {
   res.status(201).json({ mensaje: "Jugador agregado", jugador: nuevoJugador });
   
 };
+
+exports.obtenerEquipos = (req, res) => {
+  return res.status(200).json({
+    mensaje: "Equipos registrados",
+    jugadores
+  });
+};
+
+exports.borrarEquipoPorNumero= (req, res) => {
+  console.log("Tipo de contenido:", req.headers['content-type']);  
+  console.log("Cuerpo recibido:", req.body);
+  
+  const { numeroEquipo } = req.params;
+
+  const numero = parseInt(numeroEquipo);
+  const index = jugadores.findIndex(e => e.numeroEquipo === numero);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Equipo no encontrado" });
+  }
+
+  const eliminado = jugadores.splice(index, 1)[0];
+
+  return res.status(200).json({
+    mensaje: "Equipo eliminado correctamente",
+    equipoEliminado: eliminado
+  });
+};
+
+
+
