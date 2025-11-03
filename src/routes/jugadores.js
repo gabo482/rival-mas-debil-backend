e22665e3ad992fce764babcd9ac80560e20cf115
gabo482 <vcruztesci@gmail.com>
2025-11-03 14:11:43 -0600

    Se agrega borrado de jugador

--- src/routes/jugadores.js
+++ src/routes/jugadores.js
@@ -5,6 +5,8 @@ const jugadoresController = require('../controllers/jugadoresController');
 
 // Ruta POST para agregar jugador
 router.post('/agregar', jugadoresController.agregarJugador);
+router.get('/listar', jugadoresController.obtenerEquipos);
+router.delete('/borrar/:numeroEquipo', jugadoresController.borrarEquipoPorNumero);
 
 module.exports = router;
 
